66940cd9c44bd0e2f46c9cba8e53215b011859a6
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-16 14:33:32 -0300

    - Commit inicial do braia

---
+++ parimpar.py
@@ -0,0 +1,13 @@
+# projeto par ou impar
+
+# Apresentação
+print('\t\t\t -- Verificador de número\n\n')
+
+# Entradas
+n1 = int(input('informe um número: '))
+
+# Processamento e Saída
+if n1 % 2 == 0:
+    print(n1, 'é par')
+else:
+    print(n1, 'é ímpar')
